6d9e3c0933b12cae41e1c1215a80b764db3abb95
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-12 23:07:25 +0530

    Update: Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Dashboard />
     </>
   )
 }
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={index}>
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Autor</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">Title ( Year )</h2>
+                                <p className="mt-1">Genre</p>
+                                <p className="mt-1 text-gray-900">Status</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Dashboard
