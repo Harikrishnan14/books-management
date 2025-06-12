2bbe274bd709ab933dce91de690350d8d4b4abad
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-12 23:24:44 +0530

    Update: sticky navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <header className="text-gray-600 body-font">
+        <header className="text-gray-600 body-font sticky top-0 bg-white z-10 shadow-sm">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
