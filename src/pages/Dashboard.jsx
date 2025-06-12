e96e2099a2286107e299c9ab4eaa3ba1ec4bf745
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-12 23:28:42 +0530

    Update: add new button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,12 +6,24 @@ const Dashboard = () => {
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 md:py-10 py-10 mx-auto">
-                <div className="flex justify-center md:justify-end mb-12">
+                <div className="flex justify-center md:justify-end mb-12 gap-4">
                     <input
                         type="text"
                         placeholder="Search books..."
                         className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-200 focus:border-transparent w-full max-w-xs"
                     />
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-base mt-4 md:mt-0">Add New
+                        <svg
+                            className="w-4 h-4 ml-1"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                        </svg>
+                    </button>
                 </div>
                 <div className="flex flex-wrap -m-4">
                     {Array.from({ length: 12 }).map((_, index) => (
