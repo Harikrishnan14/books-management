7141df3933016d5b593040ac360203fbd7146899
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-13 13:07:40 +0530

    Update: routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { Bounce, ToastContainer } from 'react-toastify'
 import './App.css'
-import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Routes, Route } from "react-router";
+import MainLayout from './Layouts/MainLayout';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Dashboard />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,21 +26,21 @@ const Navbar = () => {
     return (
         <header className="text-gray-600 body-font sticky top-0 bg-white z-10 shadow-sm">
             <div className="container mx-auto flex flex-wrap p-5 md:flex-row md:items-center relative md:static">
-                <a href={'/'} className="flex title-font font-medium items-center text-gray-900">
+                <Link to='/' className="flex title-font font-medium items-center text-gray-900">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
                     <span className="ml-3 text-xl hidden md:inline">Books Management</span>
-                </a>
+                </Link>
                 <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden ml-auto focus:outline-none" aria-label="Open menu">
                     <svg className="w-8 h-8 text-gray-900" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
                         <path d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
                 <nav className="md:ml-auto md:mr-auto hidden md:flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">Home</a>
-                    <a className="mr-5 hover:text-gray-900">About</a>
-                    <a className="mr-5 hover:text-gray-900">Contact</a>
+                    <Link to='/' className="mr-5 hover:text-gray-900">Home</Link>
+                    <Link to='/' className="mr-5 hover:text-gray-900">About</Link>
+                    <Link to='/' className="mr-5 hover:text-gray-900">Contact</Link>
                 </nav>
                 <button className="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-base mt-4 md:mt-0">Login
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
@@ -50,18 +50,18 @@ const Navbar = () => {
                 {menuOpen && (
                     <div className="md:hidden absolute top-5 right-5 bg-gray-100 px-8 py-4 rounded-xl shadow-lg z-1000" ref={menuRef}>
                         <nav className="flex flex-col space-y-4 text-gray-900">
-                            <a href={'/'} className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
+                            <Link to='/' className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
                                 Home
-                            </a>
-                            <a href={'/'} className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
+                            </Link>
+                            <Link to='/' className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
                                 About
-                            </a>
-                            <a href={'/'} className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
+                            </Link>
+                            <Link to='/' className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
                                 Contact
-                            </a>
-                            <a href={'/'} className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
+                            </Link>
+                            <Link to='/' className="flex title-font font-medium items-center text-gray-900" onClick={() => setMenuOpen(false)}>
                                 Login
-                            </a>
+                            </Link>
                         </nav>
                     </div>
                 )}
