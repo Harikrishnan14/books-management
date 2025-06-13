d408ca628b24269ea80b77678b973928f2d135d2
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-13 12:08:51 +0530

    Update: fethcing and displaying notes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteModal from '../components/modals/DeleteModal';
 import ManageBookModal from '../components/modals/ManageBookModal';
+import axios from 'axios';
+import { CircularProgress } from "@mui/material";
+import Pagination from '@mui/material/Pagination';
 
 const Dashboard = () => {
 
     const [openDeleteModal, setOpenDeleteModal] = useState(false);
     const [openModal, setOpenModal] = useState(false);
+    const [booksData, setBooksData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const itemsPerPage = 10;
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = booksData.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(booksData.length / itemsPerPage);
 
     const handleOpenDeleteModal = () => setOpenDeleteModal(true);
     const handleCloseDeleteModal = () => setOpenDeleteModal(false);
     const handleOpenModal = () => setOpenModal(true);
     const handleCloseModal = () => setOpenModal(false);
+
+    const handlePageChange = (event, value) => {
+        setCurrentPage(value);
+    }
+
+    const fetchData = async () => {
+        setLoading(true)
+        try {
+            const response = await axios.get("https://crudcrud.com/api/29c302eb1cfd4d188bf74bb0a3434ffc/books")
+            if (response.data) {
+                setBooksData(response.data)
+            }
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [currentPage])
 
     return (
         <>
@@ -38,28 +76,51 @@ const Dashboard = () => {
                         </button>
                     </div>
                     <div className="flex flex-wrap -m-4">
-                        {Array.from({ length: 12 }).map((_, index) => (
-                            <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={index}>
-                                <a className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-                                </a>
-                                <div className="mt-4">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Autor</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Title ( Year )</h2>
-                                    <p className="mt-1">Genre</p>
-                                    <p className="mt-1 text-gray-900">Status</p>
-                                </div>
-                                <div className="flex flex-row gap-2 mt-3">
-                                    <button className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition hover:cursor-pointer text-sm" onClick={() => handleOpenModal("edit")}>
-                                        <EditIcon />
-                                    </button>
-                                    <button className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition hover:cursor-pointer text-sm" onClick={() => handleOpenDeleteModal()}>
-                                        <DeleteIcon />
-                                    </button>
-                                </div>
+                        {loading ? (
+                            <div className="flex justify-center items-center h-70 md:h-100 w-full">
+                                <CircularProgress />
                             </div>
-                        ))}
+                        ) : !booksData || booksData?.length === 0 ? (
+                            <div className="flex items-center justify-center w-full h-[50vh] md:h-[60vh]">
+                                <span className="text-gray-700 text-xl title-font font-medium">No Data!!!</span>
+                            </div>
+                        ) : (
+                            currentItems?.map((book) => (
+                                <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={book._id}>
+                                    <a className="block relative h-48 rounded overflow-hidden">
+                                        <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+                                    </a>
+                                    <div className="mt-4">
+                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{book?.author}</h3>
+                                        <h2 className="text-gray-900 title-font text-lg font-medium">{book?.title} ( {book?.published_at} )</h2>
+                                        <p className="mt-1">{book?.genre}</p>
+                                        <p className="mt-1 text-gray-900">
+                                            {book?.status?.charAt(0).toUpperCase() + book?.status?.slice(1)}
+                                        </p>
+                                    </div>
+                                    <div className="flex flex-row gap-2 mt-3">
+                                        <button className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition hover:cursor-pointer text-sm" onClick={() => handleOpenModal("edit")}>
+                                            <EditIcon />
+                                        </button>
+                                        <button className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition hover:cursor-pointer text-sm" onClick={() => handleOpenDeleteModal()}>
+                                            <DeleteIcon />
+                                        </button>
+                                    </div>
+                                </div>
+                            ))
+                        )}
                     </div>
+                    {!loading && booksData.length > 0 && (
+                        <div className="flex justify-center mt-10">
+                            <Pagination
+                                count={totalPages}
+                                page={currentPage}
+                                onChange={handlePageChange}
+                                color="primary"
+                                shape="rounded"
+                            />
+                        </div>
+                    )}
                 </div>
             </section>
 
